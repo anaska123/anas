05d5b252849d7b17978bb55a1996eb04a45349db
anaska123 <a98879081@gmail.com>
2025-05-16 16:00:56 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  }
-}, { timestamps: true });
+  name: { type: String, required: true },
+  url: { type: String, required: true },
+  description: { type: String, required: true }
+});
 
 module.exports = mongoose.model('Project', projectSchema);
+
 
